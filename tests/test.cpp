d849eab5085a2a552b38d3d14a220bc015c79192
tynkov <andrey.tynkov@gmail.com>
2025-05-21 04:50:24 -0400

    second task

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,26 +1,3 @@
-## Laboratory work V
-
-## Homework
-
-### Задание
-
-1. Создайте `CMakeList.txt` для библиотеки *banking*.
-```cmake
-cmake_minimum_required(VERSION 3.10)
-
-set(CMAKE_CXX_STANDARD 11)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-
-project(banking)
-
-add_library(banking STATIC Transaction.cpp Account.cpp)
-target_include_directories(banking PUBLIC ${CMAKE_CURRENT_SOURCE_DIR})
-```
-
-2. Создайте модульные тесты на классы `Transaction` и `Account`.
-    * Используйте mock-объекты.
-    * Покрытие кода должно составлять 100%.
-```cpp
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <Account.h>
@@ -82,17 +59,3 @@ TEST(Transaction, Banking){
     ASSERT_EQ(John.GetBalance(), init_balance-1000-init_fee);
     ASSERT_EQ(Harry.GetBalance(), init_balance+1000);
 }
-```
-
-3. Настройте сборочную процедуру на **TravisCI**.
-4. Настройте [Coveralls.io](https://coveralls.io/).
-
-## Links
-
-- [C++ CI: Travis, CMake, GTest, Coveralls & Appveyor](http://david-grs.github.io/cpp-clang-travis-cmake-gtest-coveralls-appveyor/)
-- [Boost.Tests](http://www.boost.org/doc/libs/1_63_0/libs/test/doc/html/)
-- [Catch](https://github.com/catchorg/Catch2)
-
-```
-Copyright (c) 2015-2021 The ISC Authors
-```
